548107e0d3d6ffce4c111048ca4168ccea030bf4
Irinnahar <irincse.bu@gmail.com>
2021-08-10 12:29:41 -0400

    analytics partially done

--- Client/Scripts/chartdata.js
+++ Client/Scripts/chartdata.js
@@ -7,6 +7,14 @@ const pieChartData = async() => {
     let labels = ['Africa', 'Asia', 'Europe', 'Latin America', 'North America'];
     let data = [2478, 5267, 734, 784, 433];
 
+    let chart = document.getElementById('chartdata');
+    let chartValue = chart.value;
+
+    for (let count = 0; count < chartValue.length; count++) {
+        console.log(chartValue[count]);
+    }
+    console.log(chartValue[0]);
+
     // DrawPieChart(canvas);
     new Chart(canvas, {
         type: 'pie',
