412cabfd2b18a0d59fa6aa9a7088b146f452fc13
RAGHAV THAKKAR <thakkarraghav2906@gmail.com>
2021-08-16 22:11:20 -0400

    updated controller

--- Server/Controllers/surveyController.js
+++ Server/Controllers/surveyController.js
@@ -12,8 +12,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.AnalyticsSurveyResponse = exports.DisplayAnalytics = exports.ExportSurveyResponse = exports.DisplaySurveyResponse = exports.DeleteSurvey = exports.ProcessTakeSurvey = exports.TakeSurvey = exports.DisplayThankYou = exports.ProcessDF = exports.DF = exports.CreateSurvey = exports.DisplaySurvey = void 0;
+exports.AnalyticsSurveyResponse = exports.DisplayAnalytics = exports.ExportSurveyResponse = exports.DisplaySurveyResponse = exports.ProcessSurvey = exports.ProcessEditSurvey = exports.EditSurvey = exports.DeleteSurvey = exports.ProcessTakeSurvey = exports.TakeSurvey = exports.DisplayThankYou = exports.ProcessDF = exports.DF = exports.CreateSurvey = exports.DisplaySurvey = void 0;
 const Survey_1 = __importDefault(require("../Models/Survey"));
+const Option_1 = __importDefault(require("../Models/Option"));
+const moment_1 = __importDefault(require("moment"));
+const questions_1 = __importDefault(require("../Models/questions"));
 const SurveyResponse_1 = __importDefault(require("../Models/SurveyResponse"));
 var chunk = require('lodash.chunk');
 const Util_1 = require("../Util");
@@ -168,6 +171,268 @@ function DeleteSurvey(req, res, next) {
     });
 }
 exports.DeleteSurvey = DeleteSurvey;
+function EditSurvey(req, res, next) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            let id = req.params.id;
+            const item = yield Survey_1.default.findOne({ _id: id }).populate({
+                path: 'questions',
+                model: 'Question',
+                populate: {
+                    path: 'optionsList',
+                    model: 'Option',
+                }
+            }).exec();
+            let startDate = moment_1.default(item.startDate).format("yyyy-MM-DD");
+            let endDate = moment_1.default(item.endDate).format("yyyy-MM-DD");
+            res.render('survey/edit', {
+                title: 'Edit Survey',
+                page: 'index',
+                data: item,
+                displayName: Util_1.UserDisplayName(req),
+                startDate: startDate,
+                endDate: endDate
+            });
+        }
+        catch (err) {
+            console.error(err);
+            res.end(err);
+        }
+    });
+}
+exports.EditSurvey = EditSurvey;
+function ProcessEditSurvey(req, res, next) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            let id = req.params.id;
+            let item = yield Survey_1.default.findOne({ _id: id }).populate({
+                path: 'questions',
+                model: 'Question',
+                populate: {
+                    path: 'optionsList',
+                    model: 'Option',
+                }
+            }).exec();
+            item.title = req.body.title;
+            item.description = req.body.description;
+            console.log("Start Date " + new Date(req.body.startDate));
+            item.startDate = new Date(req.body.startDate);
+            item.endDate = new Date(req.body.endDate);
+            if (item.type === "1") {
+                item.questions[0].question = req.body.q1;
+                item.questions[1].question = req.body.q2;
+                item.questions[2].question = req.body.q3;
+                item.questions[3].question = req.body.q4;
+                item.questions[4].question = req.body.q5;
+            }
+            else {
+                item.questions[0].question = req.body.q1;
+                item.questions[0].optionsList[0].option = req.body.q1o1;
+                item.questions[0].optionsList[1].option = req.body.q1o2;
+                item.questions[0].optionsList[2].option = req.body.q1o3;
+                item.questions[0].optionsList[3].option = req.body.q1o4;
+                item.questions[0].optionsList[0].save();
+                item.questions[0].optionsList[1].save();
+                item.questions[0].optionsList[2].save();
+                item.questions[0].optionsList[3].save();
+                item.questions[1].question = req.body.q2;
+                item.questions[1].optionsList[0].option = req.body.q2o1;
+                item.questions[1].optionsList[1].option = req.body.q2o2;
+                item.questions[1].optionsList[2].option = req.body.q2o3;
+                item.questions[1].optionsList[3].option = req.body.q2o4;
+                item.questions[1].optionsList[0].save();
+                item.questions[1].optionsList[1].save();
+                item.questions[1].optionsList[2].save();
+                item.questions[1].optionsList[3].save();
+                item.questions[2].question = req.body.q3;
+                item.questions[2].optionsList[0].option = req.body.q3o1;
+                item.questions[2].optionsList[1].option = req.body.q3o2;
+                item.questions[2].optionsList[2].option = req.body.q3o3;
+                item.questions[2].optionsList[3].option = req.body.q3o4;
+                item.questions[2].optionsList[0].save();
+                item.questions[2].optionsList[1].save();
+                item.questions[2].optionsList[2].save();
+                item.questions[2].optionsList[3].save();
+                item.questions[3].question = req.body.q4;
+                item.questions[3].optionsList[0].option = req.body.q4o1;
+                item.questions[3].optionsList[1].option = req.body.q4o2;
+                item.questions[3].optionsList[2].option = req.body.q4o3;
+                item.questions[3].optionsList[3].option = req.body.q4o4;
+                item.questions[3].optionsList[0].save();
+                item.questions[3].optionsList[1].save();
+                item.questions[3].optionsList[2].save();
+                item.questions[3].optionsList[3].save();
+                item.questions[4].question = req.body.q5;
+                item.questions[4].optionsList[0].option = req.body.q5o1;
+                item.questions[4].optionsList[1].option = req.body.q5o2;
+                item.questions[4].optionsList[2].option = req.body.q5o3;
+                item.questions[4].optionsList[3].option = req.body.q5o4;
+                item.questions[4].optionsList[0].save();
+                item.questions[4].optionsList[1].save();
+                item.questions[4].optionsList[2].save();
+                item.questions[4].optionsList[3].save();
+            }
+            item.questions[0].save();
+            item.questions[1].save();
+            item.questions[2].save();
+            item.questions[3].save();
+            item.questions[4].save();
+            item.save();
+            res.redirect('/survey');
+        }
+        catch (err) {
+            console.error(err);
+            res.end(err);
+        }
+    });
+}
+exports.ProcessEditSurvey = ProcessEditSurvey;
+function ProcessSurvey(req, res, next) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            let requiredLoginValue = req.body['requiredLogin'];
+            let requiredLogin = true;
+            if (requiredLoginValue) {
+                requiredLogin = true;
+            }
+            else {
+                requiredLogin = false;
+            }
+            let activeValue = req.body['active'];
+            let active = true;
+            if (activeValue) {
+                active = true;
+            }
+            else {
+                active = false;
+            }
+            if (req.body.inlineRadioOptions === "option1") {
+                const q1o1 = yield Option_1.default.create(new Option_1.default({ option: 'True' }));
+                const q1o2 = yield Option_1.default.create(new Option_1.default({ option: 'False' }));
+                const q2o1 = yield Option_1.default.create(new Option_1.default({ option: 'True' }));
+                const q2o2 = yield Option_1.default.create(new Option_1.default({ option: 'False' }));
+                const q3o1 = yield Option_1.default.create(new Option_1.default({ option: 'True' }));
+                const q3o2 = yield Option_1.default.create(new Option_1.default({ option: 'False' }));
+                const q4o1 = yield Option_1.default.create(new Option_1.default({ option: 'True' }));
+                const q4o2 = yield Option_1.default.create(new Option_1.default({ option: 'False' }));
+                const q5o1 = yield Option_1.default.create(new Option_1.default({ option: 'True' }));
+                const q5o2 = yield Option_1.default.create(new Option_1.default({ option: 'False' }));
+                const q1 = new questions_1.default({
+                    question: req.body.q1,
+                    optionsList: [q1o1, q1o2],
+                    type: "1"
+                });
+                const q2 = new questions_1.default({
+                    question: req.body.q2,
+                    optionsList: [q2o1, q2o2],
+                    type: "1"
+                });
+                const q3 = new questions_1.default({
+                    question: req.body.q3,
+                    optionsList: [q3o1, q3o2],
+                    type: "1"
+                });
+                const q4 = new questions_1.default({
+                    question: req.body.q4,
+                    optionsList: [q4o1, q4o2],
+                    type: "1"
+                });
+                const q5 = new questions_1.default({
+                    question: req.body.q5,
+                    optionsList: [q5o1, q5o2],
+                    type: "1"
+                });
+                const newQ1 = yield questions_1.default.create(q1);
+                const newQ2 = yield questions_1.default.create(q2);
+                const newQ3 = yield questions_1.default.create(q3);
+                const newQ4 = yield questions_1.default.create(q4);
+                const newQ5 = yield questions_1.default.create(q5);
+                const survey = new Survey_1.default({
+                    questions: [newQ1, newQ2, newQ3, newQ4, newQ5],
+                    active: active,
+                    userId: req.user,
+                    type: "1",
+                    isPublic: requiredLogin,
+                    startDate: new Date(req.body.startDate),
+                    endDate: new Date(req.body.endDate),
+                    title: req.body.title,
+                    description: req.body.description,
+                });
+                const newSurevy = yield Survey_1.default.create(survey);
+            }
+            else {
+                const q1o1 = yield Option_1.default.create(new Option_1.default({ option: req.body.q1o1 }));
+                const q1o2 = yield Option_1.default.create(new Option_1.default({ option: req.body.q1o2 }));
+                const q1o3 = yield Option_1.default.create(new Option_1.default({ option: req.body.q1o3 }));
+                const q1o4 = yield Option_1.default.create(new Option_1.default({ option: req.body.q1o4 }));
+                const q2o1 = yield Option_1.default.create(new Option_1.default({ option: req.body.q2o1 }));
+                const q2o2 = yield Option_1.default.create(new Option_1.default({ option: req.body.q2o2 }));
+                const q2o3 = yield Option_1.default.create(new Option_1.default({ option: req.body.q2o3 }));
+                const q2o4 = yield Option_1.default.create(new Option_1.default({ option: req.body.q2o4 }));
+                const q3o1 = yield Option_1.default.create(new Option_1.default({ option: req.body.q3o1 }));
+                const q3o2 = yield Option_1.default.create(new Option_1.default({ option: req.body.q3o2 }));
+                const q3o3 = yield Option_1.default.create(new Option_1.default({ option: req.body.q3o3 }));
+                const q3o4 = yield Option_1.default.create(new Option_1.default({ option: req.body.q3o4 }));
+                const q4o1 = yield Option_1.default.create(new Option_1.default({ option: req.body.q4o1 }));
+                const q4o2 = yield Option_1.default.create(new Option_1.default({ option: req.body.q4o2 }));
+                const q4o3 = yield Option_1.default.create(new Option_1.default({ option: req.body.q4o3 }));
+                const q4o4 = yield Option_1.default.create(new Option_1.default({ option: req.body.q4o4 }));
+                const q5o1 = yield Option_1.default.create(new Option_1.default({ option: req.body.q5o1 }));
+                const q5o2 = yield Option_1.default.create(new Option_1.default({ option: req.body.q5o2 }));
+                const q5o3 = yield Option_1.default.create(new Option_1.default({ option: req.body.q5o3 }));
+                const q5o4 = yield Option_1.default.create(new Option_1.default({ option: req.body.q5o4 }));
+                const q1 = new questions_1.default({
+                    question: req.body.q1,
+                    optionsList: [q1o1, q1o2, q1o3, q1o4],
+                    type: "2"
+                });
+                const q2 = new questions_1.default({
+                    question: req.body.q2,
+                    optionsList: [q2o1, q2o2, q2o3, q2o4],
+                    type: "2"
+                });
+                const q3 = new questions_1.default({
+                    question: req.body.q3,
+                    optionsList: [q3o1, q3o2, q3o3, q3o4],
+                    type: "2"
+                });
+                const q4 = new questions_1.default({
+                    question: req.body.q4,
+                    optionsList: [q4o1, q4o2, q4o3, q4o4],
+                    type: "2"
+                });
+                const q5 = new questions_1.default({
+                    question: req.body.q5,
+                    optionsList: [q5o1, q5o2, q5o3, q5o4],
+                    type: "2"
+                });
+                const newQ1 = yield questions_1.default.create(q1);
+                const newQ2 = yield questions_1.default.create(q2);
+                const newQ3 = yield questions_1.default.create(q3);
+                const newQ4 = yield questions_1.default.create(q4);
+                const newQ5 = yield questions_1.default.create(q5);
+                const survey = new Survey_1.default({
+                    questions: [newQ1, newQ2, newQ3, newQ4, newQ5],
+                    active: active,
+                    userId: req.user,
+                    type: '2',
+                    isPublic: requiredLogin,
+                    startDate: new Date(req.body.startDate),
+                    endDate: new Date(req.body.endDate),
+                    title: req.body.title,
+                    description: req.body.description,
+                });
+                const newSurevy = yield Survey_1.default.create(survey);
+            }
+            res.redirect('/survey');
+        }
+        catch (err) {
+            console.error(err);
+            res.end(err);
+        }
+    });
+}
+exports.ProcessSurvey = ProcessSurvey;
 function DisplaySurveyResponse(req, res, next) {
     return __awaiter(this, void 0, void 0, function* () {
         try {
