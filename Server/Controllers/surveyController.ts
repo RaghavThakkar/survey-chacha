38667477846d08d1ed128842ca4275c3ec6c0e2a
RAGHAV THAKKAR <thakkarraghav2906@gmail.com>
2021-08-16 22:06:42 -0400

    added ProcessTakeSurvey

--- Server/Controllers/surveyController.ts
+++ Server/Controllers/surveyController.ts
@@ -90,6 +90,30 @@ export async function DisplayThankYou(req: Request, res: Response, next: NextFun
 
 
 
+export async function ProcessTakeSurvey(req: Request, res: Response, next: NextFunction) {
+  try {
+    let id = req.params.id;
+    const survey = await Survey.findOne({ _id: id }).exec();
+
+    if (survey.isPublic && !req.isAuthenticated()) {
+      res.redirect('/login');
+      return
+    }
+    const surveyresponse = new SurveyResponse(
+      {
+        questionValue: [req.body.q1Radio, req.body.q2Radio, req.body.q3Radio, req.body.q4Radio, req.body.q5Radio],
+        survey: survey,
+        ownerId: req.user
+      }
+    );
+
+    const q1o1 = await SurveyResponse.create(surveyresponse);
+    res.redirect('/survey/thanks');
+  } catch (err) {
+    console.error(err);
+    res.end(err);
+  }
+}
 
 export async function DeleteSurvey(req: Request, res: Response, next: NextFunction) {
   try {
@@ -279,3 +303,140 @@ export async function DisplayAnalytics(
   }
 }
 
+export async function AnalyticsSurveyResponse(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    let id = req.params.id;
+    const responses = await SurveyResponse.find({ survey: objectId(id) })
+      .populate(
+        {
+          path: 'survey',
+          model: 'Survey',
+          populate: {
+            path: 'questions',
+            model: 'Question',
+            populate: {
+              path: 'optionsList',
+              model: 'Option',
+
+            }
+          }
+        })
+      .populate({
+        path: 'ownerId',
+        model: 'User',
+      })
+      .lean()
+      .exec();
+
+    const result = responses.map((d: any) => {
+      return {
+
+        q1: d.questionValue[0],
+        q2: d.questionValue[1],
+        q3: d.questionValue[2],
+        q4: d.questionValue[3],
+        q5: d.questionValue[4],
+      };
+    });
+
+    let options = [];
+    let data = [0, 0,
+      0, 0,
+      0, 0,
+      0, 0,
+      0, 0];
+    if (responses[0].survey.type === "1") {
+      console.log("type 1");
+      data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+      for (let i = 0; i < result.length; i++) {
+        if (result[i].q1 == 'True') {
+          data[0]++;
+        } else {
+          data[1]++;
+        }
+
+        if (result[i].q2 == 'True') {
+          data[2]++;
+        } else {
+          data[3]++;
+        }
+
+        if (result[i].q3 == 'True') {
+          data[4]++;
+        } else {
+          data[5]++;
+        }
+
+        if (result[i].q4 == 'True') {
+          data[6]++;
+        } else {
+          data[7]++;
+        }
+
+        if (result[i].q5 == 'True') {
+          data[8]++;
+        } else {
+          data[9]++;
+        }
+
+      }
+    } else {
+      console.log("type 2");
+      let length = 20;
+      options = [];
+      for (let i = 0; i < length; i++) {
+        data.push(0);
+        options.push("");
+      }
+
+      let currentIndex = 0;
+      for (let i = 0; i < responses[0].survey.questions.length; i++) {
+        for (let j = 0; j < responses[0].survey.questions[i].optionsList.length; j++) {
+          options[currentIndex] = responses[0].survey.questions[i].optionsList[j].option;
+          for (let z = 0; z < responses.length; z++) {
+            if (options[currentIndex] === responses[z].questionValue[i]) {
+              data[currentIndex]++;
+            }
+          }
+
+          currentIndex++;
+        }
+
+      }
+
+      console.log(options);
+      console.log(data);
+
+
+
+    }
+
+
+
+
+
+    res.render('surveyResponse/analytics', {
+      title: 'analytics',
+      page: 'index',
+      items: JSON.stringify(responses),
+      id: id,
+      data: data,
+      options: options,
+      type: responses[0].survey.type,
+      questions: [responses[0].survey.questions[0].question,
+      responses[0].survey.questions[1].question,
+      responses[0].survey.questions[2].question,
+      responses[0].survey.questions[3].question,
+      responses[0].survey.questions[4].question],
+      displayName: UserDisplayName(req),
+    });
+  } catch (err) {
+    console.error(err);
+    res.end(err);
+  }
+}
