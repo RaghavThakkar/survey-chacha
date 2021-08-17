c881a96c1c745407c26d5f3a2b2e706e87a2a905
RAGHAV THAKKAR <thakkarraghav2906@gmail.com>
2021-08-16 22:42:46 -0400

    updated code

--- Server/Controllers/surveyController.ts
+++ Server/Controllers/surveyController.ts
@@ -91,6 +91,7 @@ export async function DisplayThankYou(req: Request, res: Response, next: NextFun
 
 
 
+
 export async function DeleteSurvey(req: Request, res: Response, next: NextFunction) {
   try {
     let id = req.params.id;
@@ -112,191 +113,6 @@ export async function DeleteSurvey(req: Request, res: Response, next: NextFuncti
   }
 }
 
-
-
-export async function ProcessSurvey(req: Request, res: Response, next: NextFunction) {
-
-  try {
-
-
-    let requiredLoginValue = req.body['requiredLogin']; // This will have one of two values, 'undefined' if it wasn't checked, or 'on' if it is checked
-
-    let requiredLogin = true;
-
-    if (requiredLoginValue) {
-      requiredLogin = true;
-    } else {
-      requiredLogin = false;
-    }
-
-    let activeValue = req.body['active']; // This will have one of two values, 'undefined' if it wasn't checked, or 'on' if it is checked
-
-    let active = true;
-
-    if (activeValue) {
-      active = true;
-    } else {
-      active = false;
-    }
-
-    if (req.body.inlineRadioOptions === "option1") {
-
-      const q1o1 = await Option.create(new Option({ option: 'True' }));
-      const q1o2 = await Option.create(new Option({ option: 'False' }));
-
-      const q2o1 = await Option.create(new Option({ option: 'True' }));
-      const q2o2 = await Option.create(new Option({ option: 'False' }));
-
-      const q3o1 = await Option.create(new Option({ option: 'True' }));
-      const q3o2 = await Option.create(new Option({ option: 'False' }));
-
-      const q4o1 = await Option.create(new Option({ option: 'True' }));
-      const q4o2 = await Option.create(new Option({ option: 'False' }));
-
-      const q5o1 = await Option.create(new Option({ option: 'True' }));
-      const q5o2 = await Option.create(new Option({ option: 'False' }));
-
-      const q1 = new Question({
-        question: req.body.q1,
-        optionsList: [q1o1, q1o2],
-        type: "1"
-      });
-
-      const q2 = new Question({
-        question: req.body.q2,
-        optionsList: [q2o1, q2o2],
-        type: "1"
-      });
-
-      const q3 = new Question({
-        question: req.body.q3,
-        optionsList: [q3o1, q3o2],
-        type: "1"
-      });
-
-      const q4 = new Question({
-        question: req.body.q4,
-        optionsList: [q4o1, q4o2],
-        type: "1"
-      });
-
-      const q5 = new Question({
-        question: req.body.q5,
-        optionsList: [q5o1, q5o2],
-        type: "1"
-      });
-
-
-
-      const newQ1 = await Question.create(q1);
-      const newQ2 = await Question.create(q2);
-      const newQ3 = await Question.create(q3);
-      const newQ4 = await Question.create(q4);
-      const newQ5 = await Question.create(q5);
-
-      const survey = new Survey({
-        questions: [newQ1, newQ2, newQ3, newQ4, newQ5],
-        active: active,
-        userId: req.user,
-        type: "1",
-        isPublic: requiredLogin,
-        startDate: new Date(req.body.startDate),
-        endDate: new Date(req.body.endDate),
-        title: req.body.title,
-        description: req.body.description,
-      });
-      const newSurevy = await Survey.create(survey);
-    } else {
-
-      const q1o1 = await Option.create(new Option({ option: req.body.q1o1 }));
-      const q1o2 = await Option.create(new Option({ option: req.body.q1o2 }));
-      const q1o3 = await Option.create(new Option({ option: req.body.q1o3 }));
-      const q1o4 = await Option.create(new Option({ option: req.body.q1o4 }));
-
-      const q2o1 = await Option.create(new Option({ option: req.body.q2o1 }));
-      const q2o2 = await Option.create(new Option({ option: req.body.q2o2 }));
-      const q2o3 = await Option.create(new Option({ option: req.body.q2o3 }));
-      const q2o4 = await Option.create(new Option({ option: req.body.q2o4 }));
-
-      const q3o1 = await Option.create(new Option({ option: req.body.q3o1 }));
-      const q3o2 = await Option.create(new Option({ option: req.body.q3o2 }));
-      const q3o3 = await Option.create(new Option({ option: req.body.q3o3 }));
-      const q3o4 = await Option.create(new Option({ option: req.body.q3o4 }));
-
-      const q4o1 = await Option.create(new Option({ option: req.body.q4o1 }));
-      const q4o2 = await Option.create(new Option({ option: req.body.q4o2 }));
-      const q4o3 = await Option.create(new Option({ option: req.body.q4o3 }));
-      const q4o4 = await Option.create(new Option({ option: req.body.q4o4 }));
-
-      const q5o1 = await Option.create(new Option({ option: req.body.q5o1 }));
-      const q5o2 = await Option.create(new Option({ option: req.body.q5o2 }));
-      const q5o3 = await Option.create(new Option({ option: req.body.q5o3 }));
-      const q5o4 = await Option.create(new Option({ option: req.body.q5o4 }));
-
-
-
-      const q1 = new Question({
-        question: req.body.q1,
-        optionsList: [q1o1, q1o2, q1o3, q1o4],
-        type: "2"
-      });
-
-      const q2 = new Question({
-        question: req.body.q2,
-        optionsList: [q2o1, q2o2, q2o3, q2o4],
-        type: "2"
-      });
-
-      const q3 = new Question({
-        question: req.body.q3,
-        optionsList: [q3o1, q3o2, q3o3, q3o4],
-        type: "2"
-      });
-
-      const q4 = new Question({
-        question: req.body.q4,
-        optionsList: [q4o1, q4o2, q4o3, q4o4],
-        type: "2"
-      });
-
-      const q5 = new Question({
-        question: req.body.q5,
-        optionsList: [q5o1, q5o2, q5o3, q5o4],
-        type: "2"
-      });
-
-
-
-      const newQ1 = await Question.create(q1);
-      const newQ2 = await Question.create(q2);
-      const newQ3 = await Question.create(q3);
-      const newQ4 = await Question.create(q4);
-      const newQ5 = await Question.create(q5);
-
-      const survey = new Survey({
-        questions: [newQ1, newQ2, newQ3, newQ4, newQ5],
-        active: active,
-        userId: req.user,
-        type: '2',
-        isPublic: requiredLogin,
-        startDate: new Date(req.body.startDate),
-        endDate: new Date(req.body.endDate),
-        title: req.body.title,
-        description: req.body.description,
-      });
-      const newSurevy = await Survey.create(survey);
-    }
-
-
-
-    res.redirect('/survey');
-  } catch (err) {
-    console.error(err);
-    res.end(err);
-  }
-
-
-}
 
 
 
@@ -342,8 +158,121 @@ export async function DisplaySurveyResponse(req: Request, res: Response, next: N
 
 }
 
+export async function ExportSurveyResponse(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    let id = req.params.id;
+    const responses = await SurveyResponse.find({ survey: objectId(id) })
+      .populate(
+        {
+          path: 'survey',
+          model: 'Survey',
+          populate: {
+            path: 'questions',
+            model: 'Question',
+            populate: {
+              path: 'optionsList',
+              model: 'Option',
+
+            }
+          }
+        })
+      .populate({
+        path: 'ownerId',
+        model: 'User',
+      })
+      .lean()
+      .exec();
+    //console.log("$$$$$$$$$$" + responses.survey.questions[0].question);
+    makeCsvFile(responses, res);
+  } catch (err) {
+    console.error(err);
+    res.end(err);
+  }
+}
+
+const makeCsvFile = (data: any, res: any) => {
+
+
+  const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+  const csvWriter = createCsvWriter({
+    path: './Client/Assets/csv/data.csv',
+    header: [
+
+
+      { id: 'q1', title: data[0].survey.questions[0].question },
+      { id: 'q2', title: data[0].survey.questions[1].question },
+      { id: 'q3', title: data[0].survey.questions[2].question },
+      { id: 'q4', title: data[0].survey.questions[3].question },
+      { id: 'q5', title: data[0].survey.questions[4].question },
+    ],
+  });
+
+
+
+  const result = data.map((d: any) => {
+    return {
+
+      q1: d.questionValue[0],
+      q2: d.questionValue[1],
+      q3: d.questionValue[2],
+      q4: d.questionValue[3],
+      q5: d.questionValue[4],
+    };
+  });
+  csvWriter
+    .writeRecords(result) // returns a promise
+    .then(() => {
+      console.log(' Succefully export to csv');
+      res.download('./Client/Assets/csv/data.csv');
+    });
+};
 
 
 
 
+export async function DisplayAnalytics(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    let id = req.params.id;
+    const responses = await SurveyResponse.find({ survey: objectId(id) })
+      .populate(
+        {
+          path: 'survey',
+          model: 'Survey',
+          populate: {
+            path: 'questions',
+            model: 'Question',
+            populate: {
+              path: 'optionsList',
+              model: 'Option',
+
+            }
+          }
+        })
+      .populate({
+        path: 'ownerId',
+        model: 'User',
+      })
+      .lean()
+      .exec();
+
+    if (responses[0].survey.type === "1") {
+      let finalData = [0, 0];
+
+    } else {
+      let finalData = [0, 0, 0, 0, 0];
+    }
+
+  } catch (err) {
+    console.error(err);
+    res.end(err);
+  }
+}
 
